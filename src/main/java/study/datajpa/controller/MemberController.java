f86e516aa1c657bad448d526820fe2143e001523
sarasa0310 <dev.sarasa.0310@gmail.com>
2023-08-30 18:27:05 +0900

    Web 확장 - 도메인 클래스 컨버터 학습

---
+++ src/main/java/study/datajpa/controller/MemberController.java
@@ -0,0 +1,36 @@
+package study.datajpa.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import study.datajpa.entity.Member;
+import study.datajpa.repository.MemberRepository;
+
+import javax.annotation.PostConstruct;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberRepository memberRepository;
+
+    @GetMapping("/members/{id}")
+    public String findMember(@PathVariable("id") Long id) {
+        Member member = memberRepository.findById(id).get();
+        return member.getUsername();
+    }
+
+    // 도메인 클래스 컨버팅
+    // 간단한 경우에만 쓸 수 있고 실무에서 잘 쓰지 않음, 단순 조회용으로만 사용 가능(변경 불가)
+    @GetMapping("/members2/{id}")
+    public String findMember2(@PathVariable("id") Member member) {
+        return member.getUsername();
+    }
+
+    @PostConstruct
+    public void init() {
+        memberRepository.save(new Member("jimmy", 20));
+    }
+
+}
