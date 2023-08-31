bf07fcf4a5159d19f76d1e881a8bde29aa296bee
sarasa0310 <dev.sarasa.0310@gmail.com>
2023-08-31 18:35:46 +0900

    구현체 분석 및 새로운 엔티티 구별 방법 학습

---
+++ src/main/java/study/datajpa/entity/Item.java
@@ -0,0 +1,39 @@
+package study.datajpa.entity;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.domain.Persistable;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Item implements Persistable<String> {
+
+    @Id // @GeneratedValue 사용하지 않고 직접 id 할당할 때
+    private String id;
+
+    @CreatedDate // 처음에 createdDate가 null인것을 활용해 비효율적인 merge가 아닌, persist 호출할 수 있도록 할 수 있음
+    private LocalDateTime createdDate;
+
+    public Item(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public String getId() {
+        return id;
+    }
+
+    @Override
+    public boolean isNew() {
+        return createdDate == null;
+    }
+
+}
